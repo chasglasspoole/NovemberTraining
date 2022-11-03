d80af70e7d5c71013ede962029d51ffc8b66d8d3
chase <chase@DESKTOP-V9AAJ8M>
2022-11-02 20:47:48 -0400

    New Tests added

---
+++ my.new.project/src/test/java/NewTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void canRunTestNGTest() {
+	  //Prerequisite
+  }
+  
+  @Test
+  public void canUseVar() {
+	  String myString = "Chase";
+	  var myVarString = "Chase";
+  }
+}
