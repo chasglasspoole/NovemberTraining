5295a3c969acd23528e18d64f514ff537d3d030d
chase <chase@DESKTOP-V9AAJ8M>
2023-01-05 13:59:55 -0500

    Create project and foundation setup

---
+++ rest.api/src/test/java/foundation/TestBase.java
@@ -0,0 +1,29 @@
+package foundation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class TestBase {
+	protected DriverManager manager;
+	protected WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+		var browserType = "chrome";
+		this.manager = DriverManagerFactory.getManager(browserType);
+		this.manager.createDriver();
+		this.driver = manager.getDriver();
+		this.driver.manage().window().maximize();
+		this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@AfterMethod
+	public void cleanup() {
+		if(this.driver != null) {
+			this.manager.quitDriver();
+		}
+	}
+}
