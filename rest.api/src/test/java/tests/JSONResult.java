73500951c5ae5f82e9e456e4abc72b7efe06959e
chase <chase@DESKTOP-V9AAJ8M>
2023-01-05 19:39:32 -0500

    Adds tasks 1-6 for rest project

---
+++ rest.api/src/test/java/tests/JSONResult.java
@@ -0,0 +1,23 @@
+package tests;
+
+public class JSONResult {
+	
+	private Data data;
+	private Support support;
+	
+	public Data getData() {
+		return this.data;
+	}
+	
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public Support getSupport() {
+		return support;
+	}
+
+	public void setSupport(Support support) {
+		this.support = support;
+	}
+}
